071b234e2a931bc906d8d55fec51736c438880d0
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-04-13 17:15:06 -0500

    setup logo component and corresponding styles

--- src/components/logo.js
+++ src/components/logo.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Logo extends Component {
   render() {
     return (
-      <div className="logo-content">
-        
+      <div className="logo-main">
+        <img alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png" />
       </div>
     )
   }

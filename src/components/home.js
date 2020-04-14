c79278a0f82fbac32a81fe587d7cd8ee09f92165
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-04-14 17:48:40 -0500

    connected recentPosts to redux

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Logo from "./logo";
 import SearchBar from "./searchbar";
 import RecentPosts from "./recentPosts";
-import Results from "./results";
 
 export default class Home extends Component {
   render() {

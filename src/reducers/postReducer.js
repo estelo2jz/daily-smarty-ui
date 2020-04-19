4f7cabe00446ac137a3ba217d11f17a5a575bcaf
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-04-19 08:25:39 -0500

    added SET_RESULTS_POSTS reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,10 +1,13 @@
 import  {
-  SET_RECENT_POSTS
+  SET_RECENT_POSTS,
+  SET_RESULTS_POSTS
 } from '../actions/types';
+import results from '../components/results';
 
 const INIT_STATE = {
   posts: [],
-  recentPosts: []
+  recentPosts: [],
+  resultsPosts: []
 }
 
 export default function(state = INIT_STATE, action) {
@@ -15,7 +18,13 @@ export default function(state = INIT_STATE, action) {
         ...state, 
         recentPosts
       }
-  
+
+    case SET_RESULTS_POSTS:
+      const resultsPosts = action.payload;
+      return {
+        ...state,
+        resultsPosts
+      }
     default:
       return state;
   }
